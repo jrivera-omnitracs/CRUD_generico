980bff77b9cfdc3c9efc6efb5792b820b04cca29
jrivera-omnitracs <t_jrivera@omnitracs.com>
2015-07-03 13:01:15 -0500

    Kickoff

---
+++ CRUD_generico/CRUD_generico/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CRUD_generico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
